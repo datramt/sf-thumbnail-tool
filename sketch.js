a7d84dbb2ff37b434d70f0a5a791ac7e5dd13c51
Dan Tramte <datramt@gmail.com>
2018-11-09 14:17:25 -0500

    init upload

---
+++ sketch.js
@@ -0,0 +1,115 @@
+let logo, score, dropdownMenu, downloadThumbnail, seriesDescription, sfFont, cnv, txtbg, gradient;
+
+function preload() {
+
+  gradient = loadImage('gradients/sf-gradient.png')
+  sfFont = loadFont('raleway/Raleway-Regular.ttf')
+
+}
+
+function setup() {
+  cnv = createCanvas(1900, 1080);
+  cnv.position(0, 30);
+
+  background(80);
+  pixelDensity(1);
+
+  txtbg = createGraphics(width, height)
+  txtbg.filter(BLUR, 2);
+
+  cnv.drop(gotFile);
+
+  seriesDescription = createInput('drag & drop score')
+  seriesDescription.changed(() => {
+    generateCanvasContent()
+    console.log(seriesDescription.value())
+  })
+
+  logo = loadImage('logos/sf.png', logoLoaded)
+
+  dropdownMenu = createSelect()
+  dropdownMenu.option('score follower');
+  dropdownMenu.option('incipitsify');
+  dropdownMenu.option('mediated scores');
+
+  dropdownMenu.changed(() => {
+    switch (dropdownMenu.value()) {
+      case 'score follower':
+        gradient = loadImage('gradients/sf-gradient.png', () => {
+          logo = loadImage('logos/sf.png', logoLoaded)
+        })
+        break;
+      case 'incipitsify':
+        gradient = loadImage('gradients/inc-gradient.png', () => {
+          logo = loadImage('logos/in.png', logoLoaded)
+        })
+        break;
+      case 'mediated scores':
+        gradient = loadImage('gradients/ms-gradient.png', () => {
+          logo = loadImage('logos/ms.png', logoLoaded)
+        })
+        break;
+    }
+  })
+
+  downloadThumbnail = createButton('download thumbnail');
+  downloadThumbnail.mouseClicked(() => {
+    saveCanvas(cnv, 'thumbnail', 'png');
+  })
+}
+
+function gotFile(file) {
+  if (file.type === 'image') {
+    score = loadImage(file.data, imageLoaded)
+  } else {
+    println('Not an image file!');
+  }
+  // console.log(this.file.width());
+}
+
+function logoLoaded() {
+  generateCanvasContent()
+}
+
+function imageLoaded() {
+  generateCanvasContent()
+}
+
+function generateCanvasContent() {
+  textFont(sfFont);
+  fill(255);
+  noStroke();
+  textSize(135.14);
+  //completely eliminate contents of off-canvas graphics
+  txtbg.noCanvas()
+  txtbg = createGraphics(width, height)
+  txtbg.textFont(sfFont);
+  txtbg.background(255, 0);
+  txtbg.noStroke();
+  txtbg.textSize(135.14);
+  txtbg.text(seriesDescription.value(), 50, 150);
+  txtbg.filter(BLUR, 5);
+
+  //if a score has been dragged onto canvas, display it
+  if (score) {
+    image(score, 0, 0, width, height);
+    blendMode(MULTIPLY);
+  }
+  //MULTIPLY colors of gradient with colors of score
+
+  image(gradient, 0, 0, width, height)
+
+  //REVERT blend mode back to normal
+  blendMode(NORMAL);
+
+  //DRAW BLURRED TEXT
+  image(txtbg, 5, 5)
+
+  //DRAW LOGO
+  image(logo, 0, height - 600, 600, 600)
+  textAlign(LEFT);
+
+  //DRAW TEXT
+  text(seriesDescription.value(), 50, 150);
+
+}
